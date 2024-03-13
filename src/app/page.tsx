61afb39a2c575c42fbc311a5525f48c682578362
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2024-03-13 02:49:45 -0700

    dev working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,208 @@
-import Image from "next/image";
+'use client';
+
+import type Capsule from '@usecapsule/web-sdk';
+import { CapsuleEthersSigner } from '@usecapsule/ethers-v6-integration';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+  const [capsule, setCapsule] = useState<Capsule>();
+  const [email, setEmail] = useState<string>('');
+  const [signature, setSignature] = useState<string>();
+  const [verificationCode, setVerificationCode] = useState<string>('');
+  const [passkeyCreationUrl, setPasskeyCreationUrl] = useState<string>('');
+  const [walletAddress, setWalletAddress] = useState<string | undefined>('');
+  const [recoverySecret, setRecoverySecret] = useState<string>('');
+  const [passkeyLoginUrl, setPasskeyLoginUrl] = useState<string>('');
+  const [userIsLoggedIn, setUserIsLoggedIn] = useState<boolean>(false);
+  const [messageToSign, setMessageToSign] = useState<string>('');
+
+  const CAPSULE_API_KEY = process.env.NEXT_PUBLIC_CAPSULE_API_KEY;
+  if (!CAPSULE_API_KEY) {
+    throw new Error("CAPSULE_API_KEY is undefined");
+  }
+
+  async function loadCapsule() {
+    let loadedInstance;
+    if (!capsule) {
+      const CapsuleModule = await import("@usecapsule/web-sdk");
+      loadedInstance = new CapsuleModule.default(
+        CapsuleModule.Environment.DEVELOPMENT,
+        CAPSULE_API_KEY
+      );
+      setCapsule(loadedInstance);
+      console.log('Capsule loaded');
+    }
+  }
+
+  useEffect(() => {
+    loadCapsule();
+  });
+
+  const checkIfLoggedIn = async (): Promise<void> => {
+    if (!capsule) {
+      throw new Error('Capsule not instantiated');
+    }
+    const loggedIn = await capsule.isFullyLoggedIn();
+    setUserIsLoggedIn(loggedIn);
+  }
+
+  // capsule.checkIfUserExists(email) can be used to determine if create or login should be
+  // called for a given email address
+  const createAccount = async (): Promise<void> => {
+    if (!capsule) {
+      throw new Error('Capsule not instantiated');
+    }
+    await capsule.createUser(email);
+  }
+
+  const verifyEmail = async (): Promise<void> => {
+    if (!capsule) {
+      throw new Error('Capsule not instantiated');
+    }
+    const url = await capsule.verifyEmail(verificationCode);
+    setPasskeyCreationUrl(url);
+    window.open(url, 'popup', 'popup=true,width=400,height=500');
+    const recoverySecret = await capsule.waitForPasskeyAndCreateWallet();
+    setWalletAddress(Object.values(capsule.getWallets())[0].address);
+    setRecoverySecret(recoverySecret);
+  }
+
+  const initiateLogin = async (): Promise<void> => {
+    if (!capsule) {
+      throw new Error('Capsule not instantiated');
+    }
+    const url = await capsule.initiateUserLogin(email);
+    setPasskeyLoginUrl(url);
+    window.open(url, 'popup', 'popup=true,width=400,height=500');
+    await capsule.waitForLoginAndSetup();
+    setWalletAddress(Object.values(capsule.getWallets())[0].address);
+  }
+
+  const signMessage = async (message: string) => {
+    if (!capsule) {
+      throw new Error('Capsule not instantiated');
+    }
+    const signer = new CapsuleEthersSigner(capsule);
+    const signature = await signer.signMessage(message);
+    setSignature(signature);
+    setWalletAddress(Object.values(capsule.getWallets())[0].address);
+  };
+
+  const logout = async () => {
+    if (!capsule) {
+      throw new Error('Capsule not instantiated');
+    }
+    await capsule.logout();
+  };
+
+  return capsule ? 
+    <main className="m-5 flex flex-col gap-4">
+      <button
+        className="m-2 rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        onClick={() =>
+          checkIfLoggedIn()
+        }
+      >
+        Check if User Is Logged In
+      </button>
+      <p className="m-2">
+        {userIsLoggedIn ? 'User is logged in' : 'User is not logged in'}
+      </p>
+      <input
+        className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-5 pt-5 backdrop-blur-2xl dark:bg-zinc-800/30 dark:from-inherit lg:bg-gray-200 lg:p-4"
+        id="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter your email"
+      />
+      <button
+        className="m-2 rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        onClick={() =>
+          createAccount()
+        }
+      >
+        Create Account
+      </button>
+      <input
+        className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-5 pt-5 backdrop-blur-2xl dark:bg-zinc-800/30 dark:from-inherit lg:bg-gray-200 lg:p-4"
+        id="verifcationCode"
+        name="verificationCode"
+        value={verificationCode}
+        onChange={(e) => setVerificationCode(e.target.value)}
+        placeholder="Enter the verification code"
+      />
+      <button
+        className="m-2 rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        onClick={() =>
+          verifyEmail()
+        }
+      >
+        Verify Email
+      </button>
+      {passkeyCreationUrl &&
+        <p className="m-2">
+          Passkey Creation URL: {passkeyCreationUrl}
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      }
+      {recoverySecret &&
+        <p className="m-2">
+          Recovery Secret: {recoverySecret}
+        </p>
+      }
+      {walletAddress &&
+        <p className="m-2">
+          Wallet Address: {walletAddress}
+        </p>
+      }
+      <input
+        className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-5 pt-5 backdrop-blur-2xl dark:bg-zinc-800/30 dark:from-inherit lg:bg-gray-200 lg:p-4"
+        id="messageToSign"
+        name="messageToSign"
+        value={messageToSign}
+        onChange={(e) => setMessageToSign(e.target.value)}
+        placeholder="Message to sign"
+      />
+      <button
+        className="m-2 rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        onClick={() =>
+          signMessage("hi")
+        }
+      >
+        Sign Message
+      </button>
+      {signature &&
+        <p className="m-2">
+          Signature: {signature}
+        </p>
+      }
+      <input
+        className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-5 pt-5 backdrop-blur-2xl dark:bg-zinc-800/30 dark:from-inherit lg:bg-gray-200 lg:p-4"
+        id="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter your email"
+      />
+      <button
+        className="m-2 rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        onClick={() =>
+          initiateLogin()
+        }
+      >
+        Initiate Login
+      </button>
+      {passkeyLoginUrl &&
+        <p className="m-2">
+          Passkey Login URL: {passkeyLoginUrl}
+        </p>
+      }
+      <button
+        onClick={logout}
+        className="m-2 rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      >
+        Logout
+      </button>
+    </main> : <p className="text-lg">Loading...</p>;
+  ;
 }
