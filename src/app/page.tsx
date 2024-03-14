21402e5822c50eb860261e79e51586dcc5351f01
nsquare3 <113948443+nsquare3@users.noreply.github.com>
2024-03-14 00:39:40 -0700

    improve error message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 
   const CAPSULE_API_KEY = process.env.NEXT_PUBLIC_CAPSULE_API_KEY;
   if (!CAPSULE_API_KEY) {
-    throw new Error('CAPSULE_API_KEY is undefined');
+    throw new Error('NEXT_PUBLIC_CAPSULE_API_KEY is undefined');
   }
 
   async function loadCapsule() {
